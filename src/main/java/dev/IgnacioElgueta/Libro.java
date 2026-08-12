154678249088b2eb52edcfeb668090bc68b164e3
IgnacioElgueta <i.elgueta@duocuc.cl>
2026-08-12 13:24:57 -0400

    First commit

---
+++ src/main/java/dev/IgnacioElgueta/Libro.java
@@ -0,0 +1,42 @@
+package dev.IgnacioElgueta;
+
+public class Libro {
+    private String title;
+    private int year;
+    private String author;
+    private String isbn;
+
+    public Libro() {
+        this.title = "Sin definir";
+        this.year = 2026;
+        this.author = "Anonimo";
+        this.isbn = "";
+    }
+
+    public boolean prestamo(String prestamo) {
+        return true;
+    }
+
+    public boolean disponibilidad(String disponibilidad) {
+        return true;
+    }
+
+    public void devolver(String fechaDevoluvion) {
+
+    }
+
+    private void isbn() {
+
+    }
+
+    private void author() {
+
+    }
+
+    private void title() {
+
+    }
+    private void year() {
+
+    }
+}
