cc7e0a573ae5977705856ce66b243dcc0a71eb9b
IgnacioElgueta <i.elgueta@duocuc.cl>
2026-08-17 00:50:41 -0400

    Terminación de Biblioteca

--- src/main/java/dev/IgnacioElgueta/Biblioteca/Prestamo.java
+++ src/main/java/dev/IgnacioElgueta/Biblioteca/Prestamo.java
@@ -1,4 +1,27 @@
 package dev.IgnacioElgueta.Biblioteca;
 
 public class Prestamo {
+   private String nombreEstudiante;
+   private String tituloLibro;
+   private String fechaPrestamo;
+   private boolean devuelto;
+
+   public Prestamo(String nombreEstudiante, String tituloLibro, String fechaPrestamo){
+        this.nombreEstudiante = nombreEstudiante;
+        this.tituloLibro = tituloLibro;
+        this.fechaPrestamo = fechaPrestamo;
+        this.devuelto = false;
+   }
+
+   public void registrarDevolucion() {
+       this.devuelto = true;
+   }
+
+   public String obtenerInformacion() {
+       return "Estudiante: " + this.nombreEstudiante + " Libro: " + this.tituloLibro + " Devuelto: " + this.devuelto;
+   }
+
+   public boolean estaDevuelto() {
+       return this.devuelto;
+   }
 }
